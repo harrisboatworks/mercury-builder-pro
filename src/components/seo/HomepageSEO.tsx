076642b58dfcd07251e8299d64c5af32bf306084
Jay Harris <jayharris@Jays-MacBook-Air.local>
2026-07-10 07:59:17 -0400

    fix: deduplicate global review schema

--- src/components/seo/HomepageSEO.tsx
+++ src/components/seo/HomepageSEO.tsx
@@ -1,6 +1,5 @@
 import { Helmet } from '@/lib/helmet';
 import { SITE_URL } from '@/lib/site';
-import { BUSINESS_SAME_AS } from '@/lib/companyInfo';
 
 export function HomepageSEO() {
   const structuredData = {
@@ -16,82 +15,6 @@ export function HomepageSEO() {
         "about": { "@id": "https://www.mercuryrepower.ca/#organization" },
         "primaryImageOfPage": { "@id": "https://www.mercuryrepower.ca/#logo" },
         "inLanguage": "en-CA"
-      },
-      {
-        "@type": "Organization",
-        "@id": "https://www.mercuryrepower.ca/#organization",
-        "name": "Harris Boat Works",
-        "alternateName": "HBW",
-        "legalName": "Harris Boat Works",
-        "url": "https://www.harrisboatworks.ca/",
-        "logo": {
-          "@type": "ImageObject",
-          "@id": "https://www.mercuryrepower.ca/#logo",
-          "url": "https://www.mercuryrepower.ca/pwa-512x512.png",
-          "caption": "Harris Boat Works"
-        },
-        "foundingDate": "1947",
-        "founder": { "@type": "Person", "name": "Harris family" },
-        "description": "Third-generation family marina on Rice Lake, Ontario. Mercury Marine Premier Dealer (Mercury dealer since 1965) and Legend Boats dealer.",
-        "telephone": "+1-905-342-2153",
-        "email": "info@harrisboatworks.ca",
-        "address": {
-          "@type": "PostalAddress",
-          "streetAddress": "5369 Harris Boat Works Rd",
-          "addressLocality": "Gores Landing",
-          "addressRegion": "ON",
-          "postalCode": "K0K 2E0",
-          "addressCountry": "CA"
-        },
-        "sameAs": BUSINESS_SAME_AS,
-        "parentOrganization": {
-          "@type": "Organization",
-          "name": "Harris Boat Works",
-          "url": "https://www.harrisboatworks.ca/"
-        }
-      },
-      {
-        "@type": ["LocalBusiness", "Store", "AutoRepair"],
-        "@id": "https://www.mercuryrepower.ca/#localbusiness",
-        "name": "Harris Boat Works",
-        "image": "https://www.mercuryrepower.ca/pwa-512x512.png",
-        "url": "https://www.harrisboatworks.ca/",
-        "telephone": "+1-905-342-2153",
-        "email": "info@harrisboatworks.ca",
-        "parentOrganization": { "@id": "https://www.mercuryrepower.ca/#organization" },
-        "address": {
-          "@type": "PostalAddress",
-          "streetAddress": "5369 Harris Boat Works Rd",
-          "addressLocality": "Gores Landing",
-          "addressRegion": "ON",
-          "postalCode": "K0K 2E0",
-          "addressCountry": "CA"
-        },
-        "geo": {
-          "@type": "GeoCoordinates",
-          "latitude": 44.1456,
-          "longitude": -78.2542
-        },
-        "areaServed": [
-          { "@type": "AdministrativeArea", "name": "Rice Lake" },
-          { "@type": "AdministrativeArea", "name": "Kawartha Lakes" },
-          { "@type": "State", "name": "Ontario" },
-          { "@type": "Country", "name": "Canada" }
-        ],
-        "makesOffer": [
-          { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Mercury outboard repower" } },
-          { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Mercury & MerCruiser marine repair" } },
-          { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Winterization and spring launch" } },
-          { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Outdoor boat storage with professional shrinkwrap" } },
-          { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "New Legend boat sales" } },
-          { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Boat rentals" } }
-        ],
-        "brand": [
-          { "@type": "Brand", "name": "Mercury Marine" },
-          { "@type": "Brand", "name": "Legend Boats" }
-        ],
-        "award": "Mercury Marine Premier Dealer",
-        "sameAs": BUSINESS_SAME_AS
       },
       {
         "@type": "Service",
