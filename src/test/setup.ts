11c370a52c4b84aca1a387a09d3020b56c2f6c38
Jay Harris <harrisboatworks@hotmail.com>
2026-08-28 07:36:30 -0400

    test: validate Radix UI upgrade interactions

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,29 +1,63 @@
 import "@testing-library/jest-dom";
 
 if (typeof window !== "undefined") {
-Object.defineProperty(window, "matchMedia", {
-  writable: true,
-  value: (query: string) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: () => {},
-    removeListener: () => {},
-    addEventListener: () => {},
-    removeEventListener: () => {},
-    dispatchEvent: () => {},
-  }),
-});
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: (query: string) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: () => {},
+      removeListener: () => {},
+      addEventListener: () => {},
+      removeEventListener: () => {},
+      dispatchEvent: () => {},
+    }),
+  });
 
-// ResizeObserver polyfill (used by Radix Slider)
-class ResizeObserverPolyfill {
-  observe() {}
-  unobserve() {}
-  disconnect() {}
+  class ResizeObserverPolyfill {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  }
+
+  (window as unknown as { ResizeObserver: typeof ResizeObserverPolyfill }).ResizeObserver =
+    (window as unknown as { ResizeObserver?: typeof ResizeObserverPolyfill }).ResizeObserver ||
+    ResizeObserverPolyfill;
+
+  const elementPrototype = window.Element.prototype;
+
+  if (typeof elementPrototype.hasPointerCapture !== "function") {
+    elementPrototype.hasPointerCapture = () => false;
+  }
+  if (typeof elementPrototype.setPointerCapture !== "function") {
+    elementPrototype.setPointerCapture = () => {};
+  }
+  if (typeof elementPrototype.releasePointerCapture !== "function") {
+    elementPrototype.releasePointerCapture = () => {};
+  }
+  if (typeof elementPrototype.scrollIntoView !== "function") {
+    elementPrototype.scrollIntoView = () => {};
+  }
+
+  if (typeof window.PointerEvent !== "function") {
+    class PointerEventPolyfill extends MouseEvent {
+      readonly pointerId: number;
+      readonly pointerType: string;
+      readonly isPrimary: boolean;
+
+      constructor(type: string, init: PointerEventInit = {}) {
+        super(type, init);
+        this.pointerId = init.pointerId ?? 1;
+        this.pointerType = init.pointerType ?? "mouse";
+        this.isPrimary = init.isPrimary ?? true;
+      }
+    }
+
+    Object.defineProperty(window, "PointerEvent", {
+      configurable: true,
+      writable: true,
+      value: PointerEventPolyfill,
+    });
+  }
 }
-(window as unknown as { ResizeObserver: typeof ResizeObserverPolyfill }).ResizeObserver =
-  (window as unknown as { ResizeObserver?: typeof ResizeObserverPolyfill }).ResizeObserver ||
-  ResizeObserverPolyfill;
-}
-
-
